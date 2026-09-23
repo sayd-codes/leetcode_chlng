0458ec232dba363091f4b538757dfda5220d5b6f
Saurabh <sayd17866@gmail.com>
2026-09-23 15:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (96.96%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def isValid(self, s):
+        
+
+        stack = []
+
+        for ch in s:
+
+            
+            if ch in "([{":
+                stack.append(ch)
+
+            
+            else:
+
+                if not stack:
+                    return False
+
+                top = stack.pop()
+
+                if ch == ')' and top != '(':
+                    return False
+
+                if ch == ']' and top != '[':
+                    return False
+
+                if ch == '}' and top != '{':
+                    return False
+
+        return len(stack) == 0
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
